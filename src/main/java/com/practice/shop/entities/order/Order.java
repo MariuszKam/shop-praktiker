eed367b967838115331c60c1f31c4a87b442440b
MariuszKam <87607813+MariuszKam@users.noreply.github.com.>
2025-03-16 19:06:21 +0100

    Add Constructor&Getters/Setters

--- src/main/java/com/practice/shop/entities/order/Order.java
+++ src/main/java/com/practice/shop/entities/order/Order.java
@@ -12,4 +12,52 @@ public class Order {
     private OrderStatus orderStatus;
     private Customer customer;
     private Payment payment;
+
+    public Order(int id, List<Product> products, OrderStatus orderStatus, Customer customer, Payment payment) {
+        this.id = id;
+        this.products = products;
+        this.orderStatus = orderStatus;
+        this.customer = customer;
+        this.payment = payment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
 }
