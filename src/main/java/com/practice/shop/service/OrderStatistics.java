6c00a73eb4460ada1f61cde9eccc7288a59c2e2a
MariuszKam <87607813+MariuszKam@users.noreply.github.com.>
2025-03-16 20:14:45 +0100

    Add logic to OrderStatistics

---
+++ src/main/java/com/practice/shop/service/OrderStatistics.java
@@ -0,0 +1,43 @@
+package com.practice.shop.service;
+
+import com.practice.shop.entities.admin.Report;
+import com.practice.shop.entities.admin.SalesAnalysis;
+import com.practice.shop.entities.order.Order;
+import com.practice.shop.entities.product.Product;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class OrderStatistics {
+
+    private final Report<Order> report;
+
+    public OrderStatistics(List<Order> orders) {
+        this.report = new SalesAnalysis(orders);
+    }
+
+    public double getAverageOrderSum() {
+        return report.getReportList()
+                .stream().mapToDouble(order -> order.getProducts().stream()
+                        .mapToDouble(Product::getPrice).sum())
+                .average().orElse(0.0);
+    }
+
+    public Order getMostExpensive() {
+        return report.getReportList().stream()
+                .max(Comparator.comparingDouble(order ->
+                        order.getProducts().stream().mapToDouble(Product::getPrice).sum()))
+                .orElse(null);
+    }
+
+    public Product getBestseller() {
+        return Objects.requireNonNull(report.getReportList().stream()
+                .flatMap(order -> order.getProducts().stream())
+                .collect(Collectors.groupingBy(product -> product, Collectors.counting()))
+                .entrySet().stream().max(Comparator.comparingLong(Map.Entry::getValue))
+                .orElse(null)).getKey();
+    }
+}
