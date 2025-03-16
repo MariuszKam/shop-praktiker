2584c149ba780482807cf6a24ebc9e7d0d79b9c0
MariuszKam <87607813+MariuszKam@users.noreply.github.com.>
2025-03-16 20:13:10 +0100

    Add Getters/Setters

--- src/main/java/com/practice/shop/entities/user/Admin.java
+++ src/main/java/com/practice/shop/entities/user/Admin.java
@@ -1,5 +1,7 @@
 package com.practice.shop.entities.user;
 
+import com.practice.shop.entities.admin.Report;
+
 import java.util.List;
 
 public class Admin extends User {
@@ -8,5 +10,13 @@ public class Admin extends User {
 
     public Admin(int id, String name, String email) {
         super(id, name, email);
+    }
+
+    public List<Report> getReports() {
+        return reports;
+    }
+
+    public void setReports(List<Report> reports) {
+        this.reports = reports;
     }
 }
