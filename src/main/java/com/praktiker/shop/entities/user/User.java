b844fca759094c829587ca6d96160d339f52f9a6
MariuszKam <87607813+MariuszKam@users.noreply.github.com.>
2025-03-17 09:31:04 +0100

    Change access modifier for id

--- src/main/java/com/praktiker/shop/entities/user/User.java
+++ src/main/java/com/praktiker/shop/entities/user/User.java
@@ -18,7 +18,7 @@ public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    protected Long id;
+    private Long id;
     private String username;
     private String password;
     private String email;
