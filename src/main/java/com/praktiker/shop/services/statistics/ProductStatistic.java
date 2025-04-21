83d6e40dc1caad2c3537479271b9e6821355f922
MariuszKam <87607813+MariuszKam@users.noreply.github.com.>
2025-04-22 00:48:20 +0200

    Added ProductStatistic

---
+++ src/main/java/com/praktiker/shop/services/statistics/ProductStatistic.java
@@ -0,0 +1,44 @@
+package com.praktiker.shop.services.statistics;
+
+import com.praktiker.shop.entities.order.Order;
+import com.praktiker.shop.entities.order.OrderItem;
+import com.praktiker.shop.entities.product.Product;
+import com.praktiker.shop.exceptions.OrderNotFoundException;
+import com.praktiker.shop.persistance.OrderRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ProductStatistic {
+
+    private OrderRepository orderRepository;
+
+    public ProductStatistic(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Product getBestseller() {
+        List<Order> orders = orderRepository.findAll();
+
+        if (orders.isEmpty()) {
+            throw new OrderNotFoundException("Could not find bestseller. Order list is empty");
+        }
+
+        return orders.stream()
+                .flatMap(order -> order.getOrderItems().stream())
+                .collect(Collectors.groupingBy(
+                        OrderItem::getProduct,
+                        Collectors.reducing(
+                                BigDecimal.ZERO,
+                                OrderItem::getQuantity,
+                                BigDecimal::add
+                        )
+                ))
+                .entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElseThrow(() -> new OrderNotFoundException("Could not find any product during bestseller calculation"));
+    }
+}
