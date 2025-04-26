aa628307b74c226789b9b794a90f3cdec8375deb
MariuszKam <87607813+MariuszKam@users.noreply.github.com.>
2025-04-26 21:04:42 +0200

    Implemented OrderStatisticsTest

---
+++ src/test/java/com/praktiker/shop/utilis/OrderItemTestFactory.java
@@ -0,0 +1,24 @@
+package com.praktiker.shop.utilis;
+
+import com.praktiker.shop.entities.order.OrderItem;
+import com.praktiker.shop.entities.product.Product;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class OrderItemTestFactory {
+
+    public static OrderItem createOrderItem(Long id, BigDecimal quantity, Product product) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setId(id);
+        orderItem.setQuantity(quantity);
+        orderItem.setProduct(product);
+        return orderItem;
+    }
+
+    public static List<OrderItem> createOrderItemList(Long id, BigDecimal quantity) {
+        Product product = ProductTestFactory.createProduct();
+        OrderItem orderItem = createOrderItem(id, quantity, product);
+        return List.of(orderItem);
+    }
+}
