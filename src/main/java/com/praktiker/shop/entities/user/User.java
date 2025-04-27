05428c9acf9b48c23641bbf34d94ea3a98541077
MariuszKam <87607813+MariuszKam@users.noreply.github.com.>
2025-04-27 16:37:13 +0200

    Added Builder

--- src/main/java/com/praktiker/shop/entities/user/User.java
+++ src/main/java/com/praktiker/shop/entities/user/User.java
@@ -2,9 +2,7 @@ package com.praktiker.shop.entities.user;
 
 import com.praktiker.shop.entities.order.Order;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -18,6 +16,8 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User implements UserDetails {
 
     @Id
