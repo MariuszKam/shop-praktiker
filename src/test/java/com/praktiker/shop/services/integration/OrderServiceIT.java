2b22419fb799fbb300426f49172d8ac2bfb1e0c5
MariuszKam <87607813+MariuszKam@users.noreply.github.com.>
2025-05-04 17:12:19 +0200

    Integrations Test

---
+++ src/test/java/com/praktiker/shop/services/integration/OrderServiceIT.java
@@ -0,0 +1,4 @@
+package com.praktiker.shop.services.integration;
+
+public class OrderServiceIT {
+}
---
+++ src/test/java/com/praktiker/shop/services/integration/services/OrderServiceIT.java
@@ -0,0 +1,4 @@
+package com.praktiker.shop.services.integration.services;
+
+public class OrderServiceIT {
+}
