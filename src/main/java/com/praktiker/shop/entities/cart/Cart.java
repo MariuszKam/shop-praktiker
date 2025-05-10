3527a0af2fafbcb9dc085afb2ec12c5ee450daa5
MariuszKam <87607813+MariuszKam@users.noreply.github.com.>
2025-05-10 23:58:16 +0200

    Cart added

---
+++ src/main/java/com/praktiker/shop/entities/cart/Cart.java
@@ -0,0 +1,28 @@
+package com.praktiker.shop.entities.cart;
+
+import com.praktiker.shop.entities.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "carts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cart {
+
+    @Id
+    private Long id;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items;
+}
---
+++ src/main/java/com/praktiker/shop/entities/cart/CartItem.java
@@ -0,0 +1,33 @@
+package com.praktiker.shop.entities.cart;
+
+import com.praktiker.shop.entities.product.Product;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "cart_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
