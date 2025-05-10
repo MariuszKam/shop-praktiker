2b305148d4fbbf953f516447bbffa994893ef9e5
MariuszKam <87607813+MariuszKam@users.noreply.github.com.>
2025-05-10 13:34:27 +0200

    Small Fix

--- src/main/java/com/praktiker/shop/controllers/product/ProductTypeController.java
+++ src/main/java/com/praktiker/shop/controllers/product/ProductTypeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@Repository("/products/type")
+@RequestMapping("/products/type")
 @RequiredArgsConstructor
 public class ProductTypeController {
 
